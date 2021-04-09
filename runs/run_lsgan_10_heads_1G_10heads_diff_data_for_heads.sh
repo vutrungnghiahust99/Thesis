e062f175bad9e4004d6d75a3677cf75f0465c202
walker <nghia.vt173284@sis.hust.edu.vn>
2021-04-10 02:05:46 +0700

    run_lsgan_10_heads_1G_10heads_diff_data_for_heads

---
+++ runs/run_lsgan_10_heads_1G_10heads_diff_data_for_heads.sh
@@ -0,0 +1,10 @@
+python -m src.train --exp_name 'lsgan_10_heads_1G_10heads_diff_data_for_heads' \
+                    --loss_name 'lsgan' \
+                    --augmentation '0' \
+                    --spec_g '0' \
+                    --spec_d '1' \
+                    --dist 'gauss' \
+                    --n_epochs '200' \
+                    --n_heads '10' \
+                    --interval '2' \
+                    --diff_data_for_heads '1'
