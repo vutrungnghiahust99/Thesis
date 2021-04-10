7c6bfb1354e48fd2aa56205d9f754260f2defc5d
walker <nghia.vt173284@sis.hust.edu.vn>
2021-04-10 09:49:15 +0700

    logs to csv

--- logs_csv.py
+++ logs_csv.py
@@ -1,5 +1,47 @@
-from src.dataset import MNIST
+import os
+import pandas as pd
 
-s = MNIST()
+root = "experiments"
 
-print(s)
+header = [
+    'lossg_mean', 'lossg_std', 'lossd_mean', 'lossd_std', 'dx_mean', 'dx_std', 'dgz_mean', 'dgz_std',
+    'fid_score',
+]
+
+INPUT = [
+    # 'gan1_10_heads',
+    # 'gan1_10_heads_1G_10heads',
+    # 'gan1_10_heads_1G_10heads_diff_data_for_heads',
+    # 'gan1_10_heads_diff_data_for_heads',
+    'lsgan_10_heads_1G_10heads',
+    'lsgan_10_heads_1G_10heads_diff_data_for_heads'
+]
+
+
+def get_numeric(s):
+    s = s.split('\n')
+    s = [x for x in s if '[' in x and ']' in x and 'accuracy' not in x and '/s' not in x]
+    result = []
+    for line in s:
+        line = line.split('[')[1]
+        line = line.split(']')[0]
+        line = line.split(',')
+        line = [float(x) for x in line]
+        result = result + line
+    assert len(result) == len(header)
+    return result
+
+for inp in INPUT:
+    int_file = os.path.join(root, inp, inp + '.txt')
+    OUTPUT = os.path.join(root, inp, inp + '.csv')
+
+    logs = open(int_file).read()
+    logs = logs.split('--------------------------------')
+    logs = logs[1:-1]
+    results = [get_numeric(x) for x in logs]
+    df = pd.DataFrame(results, columns=header)
+    # if os.path.isfile(OUTPUT):
+    #     raise RuntimeError(f'{OUTPUT} exists')
+    df.to_csv(OUTPUT, index=None)
+    print(OUTPUT)
+    print('Completed')
