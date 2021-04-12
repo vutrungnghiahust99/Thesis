32cddaed19986abd89f31624cc83d26a625d70cb
walker <nghia.vt173284@sis.hust.edu.vn>
2021-04-12 10:55:49 +0700

    gan1_base_dropout_for_d

--- runs/run_base_gan1_lsgan.sh
+++ runs/run_base_gan1_lsgan.sh
@@ -8,6 +8,17 @@ python -m src.train --exp_name 'gan1_base' \
                     --n_heads '1' \
                     --interval '2'
 
+python -m src.train --exp_name 'gan1_base_dropout_for_d' \
+                    --loss_name 'gan1' \
+                    --augmentation '0' \
+                    --spec_g '0' \
+                    --spec_d '1' \
+                    --dist 'gauss' \
+                    --n_epochs '300' \
+                    --n_heads '1' \
+                    --interval '2' \
+                    --use_dropout '1'
+
 python -m src.train --exp_name 'lsgan_base' \
                     --loss_name 'lsgan' \
                     --augmentation '0' \
