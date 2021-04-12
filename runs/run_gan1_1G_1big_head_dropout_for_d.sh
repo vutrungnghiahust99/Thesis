bb6f9c22f50f55516698523c4ca31228fca93e11
walker <nghia.vt173284@sis.hust.edu.vn>
2021-04-12 10:30:58 +0700

    baseline + dropout for d

---
+++ runs/run_gan1_1G_1big_head_dropout_for_d.sh
@@ -0,0 +1,12 @@
+python -m src.train --exp_name 'run_gan1_1G_1big_head_dropout_for_d' \
+                    --loss_name 'gan1' \
+                    --augmentation '0' \
+                    --spec_g '0' \
+                    --spec_d '1' \
+                    --dist 'gauss' \
+                    --n_epochs '300' \
+                    --n_heads '1' \
+                    --interval '2' \
+                    --diff_data_for_heads '0' \
+                    --use_big_head_d '1' \
+                    --use_dropout '1'
