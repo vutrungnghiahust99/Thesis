9fea2e2d0977809b22f66e0af9f68f112560fedb
walker <nghia.vt173284@sis.hust.edu.vn>
2021-04-12 09:42:34 +0700

    add dropout option for bighead D

--- src/models/random_forest_d_architecture.py
+++ src/models/random_forest_d_architecture.py
@@ -52,6 +52,23 @@ def create_d_big_head(use_sigmoid: bool, use_spec_norm: bool):
             Linear(use_spec_norm, 10, 1))
 
 
+def create_d_big_head_with_dropout(use_sigmoid: bool, use_spec_norm: bool):
+    """
+    Create a big head which has the capacity as 10 small heads
+    """
+    if use_sigmoid:
+        return nn.Sequential(
+            Linear(use_spec_norm, 256, 10),
+            nn.Dropout(),
+            Linear(use_spec_norm, 10, 1),
+            nn.Sigmoid())
+    else:
+        return nn.Sequential(
+            Linear(use_spec_norm, 256, 10),
+            nn.Dropout(),
+            Linear(use_spec_norm, 10, 1))
+
+
 class Discriminator(nn.Module):
     def __init__(self, use_sigmoid, use_spec_norm, img_shape=(1, 28, 28), n_heads=10, use_big_head_d=False, use_dropout=False):
         super(Discriminator, self).__init__()
@@ -71,7 +88,10 @@ class Discriminator(nn.Module):
                     setattr(self, "head_%i" % head, create_d_head_with_dropout(use_sigmoid, use_spec_norm))
         else:
             assert n_heads == 1
-            setattr(self, "head_%i" % 0, create_d_big_head(use_sigmoid, use_spec_norm))
+            if not use_dropout:
+                setattr(self, "head_%i" % 0, create_d_big_head(use_sigmoid, use_spec_norm))
+            else:
+                setattr(self, "head_%i" % 0, create_d_big_head_with_dropout(use_sigmoid, use_spec_norm))
 
     def forward(self, img, head_id):
         img_flat = img.view(img.size(0), -1)
