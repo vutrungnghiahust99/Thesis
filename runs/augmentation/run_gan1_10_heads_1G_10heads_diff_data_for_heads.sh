cc6e1b6f6cc6dc91518264ae180bb21e6e12b872
walker <nghia.vt173284@sis.hust.edu.vn>
2021-05-03 07:49:15 +0700

    change n_epochs to 1000 and interval to 8

--- runs/augmentation/run_gan1_10_heads_1G_10heads_diff_data_for_heads.sh
+++ runs/augmentation/run_gan1_10_heads_1G_10heads_diff_data_for_heads.sh
@@ -3,7 +3,7 @@ python -m src.train_v1 --exp_name 'gan1_10_heads_1G_10heads_diff_data_for_heads'
                        --augmentation '1' \
                        --aug_times '5' \
                        --dist 'gauss' \
-                       --n_epochs '300' \
+                       --n_epochs '1000' \
                        --n_heads '10' \
-                       --interval '4' \
+                       --interval '8' \
                        --diff_data_for_heads '1'
--- runs/augmentation/run_gan1_1G_1big_head.sh
+++ runs/augmentation/run_gan1_1G_1big_head.sh
@@ -3,7 +3,7 @@ python -m src.train_v1 --exp_name 'gan1_1G_1big_head' \
                        --augmentation '1' \
                        --aug_times '5' \
                        --dist 'gauss' \
-                       --n_epochs '300' \
+                       --n_epochs '1000' \
                        --n_heads '1' \
-                       --interval '4' \
+                       --interval '8' \
                        --use_big_head_d '1'
