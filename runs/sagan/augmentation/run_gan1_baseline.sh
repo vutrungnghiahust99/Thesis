e229e569f2b67f545b3e9909175078ca1b6d6ef7
walker <nghia.vt173284@sis.hust.edu.vn>
2021-05-08 23:55:12 +0700

    sagsan augmentation

---
+++ runs/sagan/augmentation/run_gan1_baseline.sh
@@ -0,0 +1,8 @@
+python -m src.train_sagan --exp_name 'gan1_baseline' \
+                          --loss_name 'gan1' \
+                          --augmentation '1' \
+                          --aug_times '5' \
+                          --dist 'gauss' \
+                          --n_epochs '1000' \
+                          --n_heads '1' \
+                          --interval '8'
