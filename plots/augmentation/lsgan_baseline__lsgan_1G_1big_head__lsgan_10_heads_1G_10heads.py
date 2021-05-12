178b55999f8740bc4ba822183a161efa5b0b57fc
walker <nghia.vt173284@sis.hust.edu.vn>
2021-05-12 09:43:58 +0700

    lsgan augmentation with mask

--- plots/augmentation/lsgan_baseline__lsgan_1G_1big_head__lsgan_10_heads_1G_10heads.py
+++ plots/augmentation/lsgan_baseline__lsgan_1G_1big_head__lsgan_10_heads_1G_10heads.py
@@ -10,7 +10,7 @@ df1 = pd.read_csv(exp1)
 df2 = pd.read_csv(exp2)
 df3 = pd.read_csv(exp3)
 # df4 = pd.read_csv(exp4)
-n = min(df1.shape[0], df2.shape[0], df3.shape[0])
+n = min(df1.shape[0], df2.shape[0], df3.shape[0], 376 // 8)
 df1 = df1.iloc[0:n, :]
 df2 = df2.iloc[0:n, :]
 df3 = df3.iloc[0:n, :]
@@ -19,14 +19,14 @@ epochs = [x * 8 for x in range(n)]
 
 fig, axs = plt.subplots(2, 3, sharex=False, sharey=False, figsize=(12, 8))
 
-n1 = 5
+n1 = 6
 n2 = n
 df1_ = df1.iloc[n1:n2, :]
 df2_ = df2.iloc[n1:n2, :]
 df3_ = df3.iloc[n1:n2, :]
 # df4_ = df4.iloc[n1:n2, :]
 epochs_ = epochs[n1:n2]
-axs[0, 0].plot(epochs_, df1_['fid_score'], 'k--', color='green', label='Baseline')
+# axs[0, 0].plot(epochs_, df1_['fid_score'], 'k--', color='green', label='Baseline')
 axs[0, 0].plot(epochs_, df2_['fid_score'], 'k:', color='blue', label='10 heads')
 axs[0, 0].plot(epochs_, df3_['fid_score'], label='10 heads + diff_data', color='black')
 # axs[0, 0].plot(epochs_, df4_['fid_score'], label='Baseline + big head', color='orange')
