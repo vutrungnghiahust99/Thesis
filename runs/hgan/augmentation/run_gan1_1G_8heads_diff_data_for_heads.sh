d972dcecb018066b992e2ec41eb3d5131c04d59c
walker <nghia.vt173284@sis.hust.edu.vn>
2021-05-16 16:14:51 +0700

    aug 2 times

--- runs/hgan/augmentation/run_gan1_1G_8heads_diff_data_for_heads.sh
+++ runs/hgan/augmentation/run_gan1_1G_8heads_diff_data_for_heads.sh
@@ -1,9 +1,9 @@
 python -m src.train_hgan --exp_name 'gan1_1G_8heads_diff_data_for_heads' \
                           --loss_name 'gan1' \
                           --augmentation '1' \
-                          --aug_times '5' \
+                          --aug_times '2' \
                           --dist 'gauss' \
                           --n_epochs '1000' \
                           --n_heads '8' \
-                          --interval '4' \
+                          --interval '2' \
                           --diff_data_for_heads '1'
