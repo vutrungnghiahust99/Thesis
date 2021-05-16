469b912fba510f0baaf5c19e88c52f6c4c8b1803
walker <nghia.vt173284@sis.hust.edu.vn>
2021-05-16 22:55:59 +0700

    train hgan architect but not good

---
+++ runs/hgan/augmentation/run_gan1_1G_8heads_sharedLayers.sh
@@ -0,0 +1,9 @@
+python -m src.train_hgan --exp_name 'gan1_1G_8heads_sharedLayers' \
+                         --loss_name 'gan1' \
+                         --augmentation '1' \
+                         --aug_times '2' \
+                         --dist 'gauss' \
+                         --n_epochs '1000' \
+                         --n_heads '8' \
+                         --interval '2' \
+                         --use_shared_layers '1'
