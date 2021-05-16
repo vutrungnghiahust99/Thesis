da5add37cbee36d500969aafff744e055349d7da
walker <nghia.vt173284@sis.hust.edu.vn>
2021-05-16 11:00:16 +0700

    cifar10 64 test dataset

--- src/dataset.py
+++ src/dataset.py
@@ -24,3 +24,53 @@ class MNIST(Dataset):
 
     def __len__(self) -> int:
         return len(self.data)
+
+
+class CIFAR10(Dataset):
+    def __init__(
+            self,
+            root: str,
+            transform=None):
+
+        super(CIFAR10, self).__init__()
+        s = torch.load(root)
+        self.data, self.labels, self.mapping = s['data'], s['labels'], s['mapping']
+        self.transform = transform
+
+    def __getitem__(self, index: int):
+        img, target = self.data[index], self.labels[index]
+
+        img = Image.fromarray(img)
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        return img, target, self.mapping[index]
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+
+class CIFAR10_TEST(Dataset):
+    def __init__(
+            self,
+            root: str,
+            transform=None):
+
+        super(CIFAR10, self).__init__()
+        s = torch.load(root)
+        self.data, self.labels = s['data'], s['labels']
+        self.transform = transform
+
+    def __getitem__(self, index: int):
+        img, target = self.data[index], self.labels[index]
+
+        img = Image.fromarray(img)
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        return img, target
+
+    def __len__(self) -> int:
+        return len(self.data)
