993b51fa211d4f4548412f53723788519e07ea11
walker <nghia.vt173284@sis.hust.edu.vn>
2021-05-16 16:14:19 +0700

    aug 2 times

--- runs/hgan/augmentation/run_gan1_1G_8heads.sh
+++ runs/hgan/augmentation/run_gan1_1G_8heads.sh
@@ -1,8 +1,8 @@
 python -m src.train_hgan --exp_name 'gan1_1G_8heads' \
                          --loss_name 'gan1' \
                          --augmentation '1' \
-                         --aug_times '5' \
+                         --aug_times '2' \
                          --dist 'gauss' \
                          --n_epochs '1000' \
                          --n_heads '8' \
-                         --interval '4'
+                         --interval '2'
