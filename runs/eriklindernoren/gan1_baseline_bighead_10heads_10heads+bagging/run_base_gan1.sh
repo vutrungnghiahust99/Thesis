fe115d85462b3ee7b60b47345d68c2af3a8ece32
walker <nghia.vt173284@sis.hust.edu.vn>
2021-05-17 18:10:21 +0700

    re-orgnize

---
+++ runs/eriklindernoren/gan1_baseline_bighead_10heads_10heads+bagging/run_base_gan1.sh
@@ -0,0 +1,10 @@
+python -m src.train --exp_name 'gan1_base' \
+                    --loss_name 'gan1' \
+                    --augmentation '0' \
+                    --spec_g '0' \
+                    --spec_d '1' \
+                    --dist 'gauss' \
+                    --n_epochs '300' \
+                    --n_heads '1' \
+                    --interval '2'
+                    
