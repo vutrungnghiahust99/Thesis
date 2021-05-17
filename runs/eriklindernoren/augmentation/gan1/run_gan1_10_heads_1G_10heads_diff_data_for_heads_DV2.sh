7f5dbe54b755f5d68a6bef9c3dba75a3454e985f
walker <nghia.vt173284@sis.hust.edu.vn>
2021-05-17 18:19:13 +0700

    seperate run files

--- runs/eriklindernoren/augmentation/gan1/run_gan1_10_heads_1G_10heads_diff_data_for_heads_DV2.sh
+++ runs/eriklindernoren/augmentation/gan1/run_gan1_10_heads_1G_10heads_diff_data_for_heads_DV2.sh
@@ -1,8 +1,10 @@
-python -m src.train_v1 --exp_name 'gan1_10_heads_1G_10heads' \
+python -m src.train_v1 --exp_name 'gan1_10_heads_1G_10heads_diff_data_for_heads_DV2' \
                        --loss_name 'gan1' \
                        --augmentation '1' \
                        --aug_times '5' \
                        --dist 'gauss' \
                        --n_epochs '1000' \
                        --n_heads '10' \
-                       --interval '8'
+                       --interval '8' \
+                       --diff_data_for_heads '1' \
+                       --use_d_v2 '1'
