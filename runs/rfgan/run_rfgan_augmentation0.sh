4f0e2f30523fe17b8e292d9210f50f95560d6aa9
walker <nghia.vt173284@sis.hust.edu.vn>
2021-05-23 02:01:46 +0700

    add run with mask

--- runs/rfgan/run_rfgan_augmentation0.sh
+++ runs/rfgan/run_rfgan_augmentation0.sh
@@ -4,9 +4,24 @@ python -m src.train.rfgan --exp_name rfgan_lossgan1_bagging0_augmentation0_10hea
                           --n_epochs '401' \
                           --interval '4'
 
+python -m src.train.rfgan --exp_name rfgan_lossgan1_bagging0_augmentation0_10heads_usemaskD \
+                          --loss_name 'gan1' \
+                          --n_heads '10' \
+                          --n_epochs '401' \
+                          --interval '4' \
+                          --use_mask_d '1'
+
 python -m src.train.rfgan --exp_name rfgan_lossgan1_bagging1_augmentation0_10heads \
                           --loss_name 'gan1' \
                           --n_heads '10' \
                           --n_epochs '401' \
                           --interval '4' \
                           --bagging '1'
+
+python -m src.train.rfgan --exp_name rfgan_lossgan1_bagging1_augmentation0_10heads_usemaskD \
+                          --loss_name 'gan1' \
+                          --n_heads '10' \
+                          --n_epochs '401' \
+                          --interval '4' \
+                          --bagging '1' \
+                          --use_mask_d '1'
