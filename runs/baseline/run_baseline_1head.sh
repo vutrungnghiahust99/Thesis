aeca8b43626caff4e7a5f2fb4ebde6cbcfca88a2
walker <nghia.vt173284@sis.hust.edu.vn>
2021-05-23 14:00:08 +0700

    add baseline 1 head

--- runs/baseline/run_baseline_1head.sh
+++ runs/baseline/run_baseline_1head.sh
@@ -2,11 +2,11 @@
 python -m src.train.baseline_1head --exp_name 'baseline1head_lossgan1_augmentation0_usebighead0' \
                                    --loss_name 'gan1' \
                                    --use_big_head '0' \
-                                   --n_epochs '401' \
+                                   --n_epochs '1001' \
                                    --interval '4'
 
-python -m src.train.baseline_1head --exp_name 'baseline1head_lossgan1_augmentation1_usebighead0' \
+python -m src.train.baseline_1head --exp_name 'baseline1head_lossgan1_augmentation0_usebighead1' \
                                    --loss_name 'gan1' \
                                    --use_big_head '1' \
-                                   --n_epochs '401' \
+                                   --n_epochs '1001' \
                                    --interval '4'
