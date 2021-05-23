f06ed20d8297cd123a500f7b61c3f3b53ea077b0
walker <nghia.vt173284@sis.hust.edu.vn>
2021-05-23 16:09:25 +0700

    augmentation

--- runs/rfgan/run_rfgan_augmentation1.sh
+++ runs/rfgan/run_rfgan_augmentation1.sh
@@ -1,5 +1,5 @@
 # Do not use bagging
-python -m src.train.rfgan --exp_name rfgan_lossgan1_bagging0_augmentation0_10heads_usemaskD \
+python -m src.train.rfgan --exp_name rfgan_lossgan1_bagging0_augmentation1_10heads_usemaskD \
                           --loss_name 'gan1' \
                           --n_heads '10' \
                           --n_epochs '401' \
@@ -9,7 +9,7 @@ python -m src.train.rfgan --exp_name rfgan_lossgan1_bagging0_augmentation0_10hea
                           --aug_times '2'
 
 # use bagging
-python -m src.train.rfgan --exp_name rfgan_lossgan1_bagging1_augmentation0_10heads_usemaskD \
+python -m src.train.rfgan --exp_name rfgan_lossgan1_bagging1_augmentation1_10heads_usemaskD \
                           --loss_name 'gan1' \
                           --n_heads '10' \
                           --n_epochs '401' \
