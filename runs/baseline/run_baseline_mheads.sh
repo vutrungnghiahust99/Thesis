47003bbbdd000737cd8109c2dbe3491aa00b741a
walker <nghia.vt173284@sis.hust.edu.vn>
2021-05-24 13:23:24 +0700

    baselinemheads

---
+++ runs/baseline/run_baseline_mheads.sh
@@ -0,0 +1,15 @@
+# Do not use augmentation
+python -m src.train.mdgan --exp_name 'baselinemheads_lossgan1_augmentation0' \
+                          --loss_name 'gan1' \
+                          --n_heads '10' \
+                          --n_epochs '401' \
+                          --interval '4'
+
+# Do not use augmentation
+python -m src.train.mdgan --exp_name 'baselinemheads_lossgan1_augmentation1' \
+                          --loss_name 'gan1' \
+                          --n_heads '10' \
+                          --n_epochs '401' \
+                          --interval '4' \
+                          --augmentation '1' \
+                          --aug_times '2'
