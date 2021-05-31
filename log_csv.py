431a80acc006204be88f510f0019888b066cbc3d
walker <nghia.vt173284@sis.hust.edu.vn>
2021-05-31 14:32:27 +0700

    code to save and load batch

--- log_csv.py
+++ log_csv.py
@@ -48,3 +48,37 @@ for inp in INPUT:
     df.to_csv(OUTPUT, index=None)
     print(OUTPUT)
     print('Completed')
+
+
+# import torch
+
+# from torchvision import transforms
+# from src.augmentation import Augmentation
+
+# from torchvision.utils import save_image
+
+
+# def rand_cutout(x, ratio=0.4):
+#     cutout_size = int(x.size(2) * ratio + 0.5), int(x.size(3) * ratio + 0.5)
+#     offset_x = torch.randint(0, x.size(2) + (1 - cutout_size[0] % 2), size=[x.size(0), 1, 1], device=x.device)
+#     offset_y = torch.randint(0, x.size(3) + (1 - cutout_size[1] % 2), size=[x.size(0), 1, 1], device=x.device)
+#     grid_batch, grid_x, grid_y = torch.meshgrid(
+#         torch.arange(x.size(0), dtype=torch.long, device=x.device),
+#         torch.arange(cutout_size[0], dtype=torch.long, device=x.device),
+#         torch.arange(cutout_size[1], dtype=torch.long, device=x.device),
+#     )
+#     grid_x = torch.clamp(grid_x + offset_x - cutout_size[0] // 2, min=0, max=x.size(2) - 1)
+#     grid_y = torch.clamp(grid_y + offset_y - cutout_size[1] // 2, min=0, max=x.size(3) - 1)
+#     mask = torch.ones(x.size(0), x.size(2), x.size(3), dtype=x.dtype, device=x.device)
+#     mask[grid_batch, grid_x, grid_y] = 0
+#     x = x * mask.unsqueeze(1)
+#     return x
+
+# s = torch.load('batch.pth')
+# s = torch.cat((s, s), dim=0)
+# s = s[0:100]
+
+# s1 = rand_cutout(s)
+
+# save_image(s, 'origin.png', nrow=10, normalize=True)
+# save_image(s1, 'augmentation.png', nrow=10, normalize=True)
