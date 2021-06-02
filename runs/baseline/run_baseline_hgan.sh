ef23f60fb1db27bdb3867bef17bacd03f0491415
walker <nghia.vt173284@sis.hust.edu.vn>
2021-06-02 18:49:09 +0700

    run with 3 and 5 heads

--- runs/baseline/run_baseline_hgan.sh
+++ runs/baseline/run_baseline_hgan.sh
@@ -4,6 +4,16 @@ python -m src.train.hgan --exp_name 'baselinehgan_losshgan_augmentation0' \
                          --n_epochs '401' \
                          --interval '4'
 
+python -m src.train.hgan --exp_name 'baselinehgan_losshgan_5heads_augmentation0' \
+                         --n_heads '5' \
+                         --n_epochs '401' \
+                         --interval '4'
+
+python -m src.train.hgan --exp_name 'baselinehgan_losshgan_3heads_augmentation0' \
+                         --n_heads '3' \
+                         --n_epochs '401' \
+                         --interval '4'
+
 # Use augmentation
 python -m src.train.hgan --exp_name 'baselinehgan_losshgan_augmentation1' \
                          --n_heads '10' \
--- runs/baseline/run_baseline_mheads.sh
+++ runs/baseline/run_baseline_mheads.sh
@@ -5,6 +5,18 @@ python -m src.train.mdgan --exp_name 'baselinemheads_lossgan1_augmentation0' \
                           --n_epochs '401' \
                           --interval '4'
 
+python -m src.train.mdgan --exp_name 'baselinemheads_lossgan1_5heads_augmentation0' \
+                          --loss_name 'gan1' \
+                          --n_heads '5' \
+                          --n_epochs '401' \
+                          --interval '4'
+
+python -m src.train.mdgan --exp_name 'baselinemheads_lossgan1_3heads_augmentation0' \
+                          --loss_name 'gan1' \
+                          --n_heads '3' \
+                          --n_epochs '401' \
+                          --interval '4'
+
 # Use augmentation
 python -m src.train.mdgan --exp_name 'baselinemheads_lossgan1_augmentation1' \
                           --loss_name 'gan1' \
