e48805e63f97231048adfc813a467548be7d90ba
walker <nghia.vt173284@sis.hust.edu.vn>
2021-06-02 19:58:10 +0700

    450 epochs

--- runs/rfgan/run_rfgan_augmentation0.sh
+++ runs/rfgan/run_rfgan_augmentation0.sh
@@ -2,7 +2,7 @@
 python -m src.train.rfgan --exp_name rfgan_lossgan1_bagging0_augmentation0_10heads_usemaskD \
                           --loss_name 'gan1' \
                           --n_heads '10' \
-                          --n_epochs '401' \
+                          --n_epochs '453' \
                           --interval '4' \
                           --use_mask_d '1'
 
@@ -10,7 +10,7 @@ python -m src.train.rfgan --exp_name rfgan_lossgan1_bagging0_augmentation0_10hea
 python -m src.train.rfgan --exp_name rfgan_lossgan1_bagging1_augmentation0_10heads_usemaskD \
                           --loss_name 'gan1' \
                           --n_heads '10' \
-                          --n_epochs '401' \
+                          --n_epochs '453' \
                           --interval '4' \
                           --bagging '1' \
                           --use_mask_d '1'
@@ -18,7 +18,7 @@ python -m src.train.rfgan --exp_name rfgan_lossgan1_bagging1_augmentation0_10hea
 python -m src.train.rfgan --exp_name rfgan_lossgan1_bagging1_augmentation0_5heads_usemaskD \
                           --loss_name 'gan1' \
                           --n_heads '5' \
-                          --n_epochs '401' \
+                          --n_epochs '453' \
                           --interval '4' \
                           --bagging '1' \
                           --use_mask_d '1'
@@ -26,7 +26,7 @@ python -m src.train.rfgan --exp_name rfgan_lossgan1_bagging1_augmentation0_5head
 python -m src.train.rfgan --exp_name rfgan_lossgan1_bagging1_augmentation0_3heads_usemaskD \
                           --loss_name 'gan1' \
                           --n_heads '3' \
-                          --n_epochs '401' \
+                          --n_epochs '453' \
                           --interval '4' \
                           --bagging '1' \
                           --use_mask_d '1'
--- runs/rfgan/run_rfgan_augmentation1.sh
+++ runs/rfgan/run_rfgan_augmentation1.sh
@@ -2,7 +2,7 @@
 python -m src.train.rfgan --exp_name rfgan_lossgan1_bagging0_augmentation1_10heads_usemaskD \
                           --loss_name 'gan1' \
                           --n_heads '10' \
-                          --n_epochs '401' \
+                          --n_epochs '453' \
                           --interval '4' \
                           --use_mask_d '1' \
                           --augmentation '1' \
@@ -12,7 +12,7 @@ python -m src.train.rfgan --exp_name rfgan_lossgan1_bagging0_augmentation1_10hea
 python -m src.train.rfgan --exp_name rfgan_lossgan1_bagging1_augmentation1_10heads_usemaskD \
                           --loss_name 'gan1' \
                           --n_heads '10' \
-                          --n_epochs '401' \
+                          --n_epochs '453' \
                           --interval '4' \
                           --bagging '1' \
                           --use_mask_d '1' \
