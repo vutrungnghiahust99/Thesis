72bc5e0a8cf7976e6dd3d90fa5da52d9904d268f
walker <nghia.vt173284@sis.hust.edu.vn>
2021-06-15 12:10:18 +0700

    create bagging

---
+++ create_bagging.py
@@ -0,0 +1,24 @@
+import torch
+import random
+from tqdm import tqdm
+
+train = torch.load('data/mnist/MNIST/processed/training_rf.pt')
+
+N = 60000
+
+m = [[] for _ in range(N)]
+m1 = []
+
+for i in tqdm(range(100)):
+    for _ in range(N):
+        a = random.randint(0, 59999)
+        m[a].append(i)
+
+
+for i in range(N):
+    b = ','.join([str(x) for x in m[i]])
+    m1.append(b)
+
+d = (train[0], train[1], m1)
+
+torch.save(d, 'training_rf_v2.pt')
