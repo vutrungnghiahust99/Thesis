4bcf8810f016fccbca25b923bc31eec298902770
walker <nghia.vt173284@sis.hust.edu.vn>
2021-06-18 17:27:51 +0700

    run 453 epochs with vanilla gan

--- version1/runs/baseline/run_baseline_1head.sh
+++ version1/runs/baseline/run_baseline_1head.sh
@@ -2,20 +2,20 @@
 python -m src.train.baseline_1head --exp_name 'baseline1head_lossgan1_augmentation0_usebighead0' \
                                    --loss_name 'gan1' \
                                    --use_big_head '0' \
-                                   --n_epochs '1001' \
+                                   --n_epochs '453' \
                                    --interval '4'
 
 python -m src.train.baseline_1head --exp_name 'baseline1head_lossgan1_augmentation0_usebighead1' \
                                    --loss_name 'gan1' \
                                    --use_big_head '1' \
-                                   --n_epochs '1001' \
+                                   --n_epochs '453' \
                                    --interval '4'
 
 # Use augmentation
 python -m src.train.baseline_1head --exp_name 'baseline1head_lossgan1_augmentation1_usebighead0' \
                                    --loss_name 'gan1' \
                                    --use_big_head '0' \
-                                   --n_epochs '1001' \
+                                   --n_epochs '453' \
                                    --interval '4' \
                                    --augmentation '1' \
                                    --aug_times '2'
@@ -23,7 +23,7 @@ python -m src.train.baseline_1head --exp_name 'baseline1head_lossgan1_augmentati
 python -m src.train.baseline_1head --exp_name 'baseline1head_lossgan1_augmentation1_usebighead1' \
                                    --loss_name 'gan1' \
                                    --use_big_head '1' \
-                                   --n_epochs '1001' \
+                                   --n_epochs '453' \
                                    --interval '4' \
                                    --augmentation '1' \
                                    --aug_times '2'
